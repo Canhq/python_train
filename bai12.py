a578f6f27d398ac789a12066c2f72160e62f8845
Canhq <66562157+Canhq@users.noreply.github.com>
2021-01-16 12:27:53 +0700

    update

---
+++ bai12.py
@@ -0,0 +1,23 @@
+test=input('nhap so:')
+test_list=list(test)
+
+# Printing original list
+print("Original list is : " + str(test_list))
+
+# using naive method to
+# perform conversion
+for i in range(0, len(test_list)):
+    test_list[i] = int(test_list[i])
+
+# Printing modified list
+print("Modified list is : " + str(test_list))
+a=test_list[0] + test_list[1]-test_list[2] or test_list[3] or test_list[4] - test_list[5] - test_list[6] + test_list[7]
++test_list[8]
+print('so can tim la:',bin(a))
+
+
+
+
+
+
+
